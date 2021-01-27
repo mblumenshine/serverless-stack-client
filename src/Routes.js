2cfc3ab899e146f6c093822a838f7964e8aba007
Michael Blumenshine <mikoblume@gmail.com>
2021-01-26 20:33:02 -0600

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import Settings from "./containers/Settings";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 // exact on Route component ensures that only / route will go to home,
 // otherwise without exact, anything starting with / would also go to home
@@ -11,6 +18,21 @@ export default function Routes() {
             <Route exact path="/">
                 <Home />
             </Route>
+            <UnauthenticatedRoute exact path="/login">
+                <Login />
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute exact path="/signup">
+                <Signup />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/settings">
+                <Settings />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/new">
+                <NewNote />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/:id">
+                <Notes />
+            </AuthenticatedRoute>
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
--- src/config.js
+++ src/config.js
@@ -1,18 +1,20 @@
 const config = {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "notes-app-upload-blumenshine",
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://yg5ni5fxs1.execute-api.us-east-1.amazonaws.com/prod",
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_wjFOFFvFB",
-      APP_CLIENT_ID: "1ql2s3uhqt15uep2dhjp1atg4s",
-      IDENTITY_POOL_ID: "us-east-2:51a50043-7299-47e3-8774-fa84d023c033",
-    },
-  };
-  
-  export default config;
+  STRIPE_KEY: "pk_test_51I7UIFADmAjR2li3RxPCsUNHj6DDSd4yvdlan1RPlDikOLFxI5LOKOnHuuvaGVKNlL7lxMetdszQI0krOm0ucb3U00Dtuma6vS",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-blumenshine",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://yg5ni5fxs1.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_wjFOFFvFB",
+    APP_CLIENT_ID: "1ql2s3uhqt15uep2dhjp1atg4s",
+    IDENTITY_POOL_ID: "us-east-2:51a50043-7299-47e3-8774-fa84d023c033",
+  },
+};
+
+export default config;
